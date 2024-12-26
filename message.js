b3113ecc2f6152f2f19cf1522d1674bf4248895b
Mayank Rai <mayankrai.ldh@gmail.com>
2024-12-26 20:23:10 +0530

    Update message.js

--- message.js
+++ message.js
@@ -2,5 +2,5 @@
     fetch('https://cdn.jsdelivr.net/gh/gco-rai-mayank/GroupChatOn@main/Message.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('myDiv').innerHTML = data;
+            document.getElementById('myDivGroupChatOn').innerHTML = data;
         });
